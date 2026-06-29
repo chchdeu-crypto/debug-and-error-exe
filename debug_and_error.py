8db32761f928ba8a07e9d20eded11def1737a298
chaim <chchdeu@gmail.com>
2026-06-29 16:12:00 +0300

    mission_5

---
+++ debug_and_error.py
@@ -0,0 +1,52 @@
+# #mission 1
+# age = input("Enter your age: ")
+# try:
+#     next_year = int(age) + 1
+#     print("Next year you will be", next_year)
+# except ValueError:
+#     print('enter only numbers')
+
+# #mission 2
+# a = int(input("First number: "))
+# b = int(input("Second number: "))
+# try:
+#     print(a / b)
+# except ZeroDivisionError:
+#     print("cannot divide by zero")
+
+# #mission 3
+# numbers = [10, 20, 30]
+# index = int(input("Choose index: "))
+# try:
+#     print(numbers[index])
+# except IndexError:
+#     print("index not found")
+
+# #mission 4
+# prices = {
+#     "apple": 3,
+#     "banana": 5
+# }
+# item = input("Enter item: ")
+# try:
+#     print(prices[item])
+# except KeyError:
+#     print("item not found")
+
+#mission 5
+numbers = [100, 200, 300]
+try:
+    index = int(input("Choose index: "))
+    divider = int(input("Choose divider: "))
+except ValueError:
+    print("enter numbers")
+try:    
+    result = numbers[index] / divider
+    print(result)
+except IndexError:
+    print("index out of range")
+except ZeroDivisionError:
+    print("cannot divide by zero")
+except NameError:
+    print("not ")
+
